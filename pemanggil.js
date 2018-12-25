a72cf9dc3f0c68ae9cef908ba92d595f3b5d2fb8
tofikadri14 <46119901+tofikadri14@users.noreply.github.com>
2018-12-25 15:36:21 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,8 +26,7 @@ if(x=="1"){xxx="/2018/09/6-belongings-you-ought-to-neer.html"}
 if(x=="2"){xxx="/2018/09/seven-tips-for-healthy-lifestyle.html"}
 if(x=="3"){xxx="/2018/09/7-healthy-tips-for-adults.html"}
 if(x=="4"){xxx="/2018/09/3-ways-how-to-increase-your-investment.html"}
-if(x=="5"){xxx="/2018/09/the-guide-how-to-trading-bitcoin.html"}a_to_ve[i].href="https://www.safelink.cekbae.com/"+xxx+"?url=”+  
- encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
+if(x=="5"){xxx="/2018/09/the-guide-how-to-trading-bitcoin.html"}a_to_ve[i].href="https://www.safelink.cekbae.com/"+xxx+"?url=”+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
